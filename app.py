e12c040a5c5da47f3a8d8915f9bc38e0c6792019
lyvahne <62095707+Lyvahne@users.noreply.github.com>
2022-07-16 17:46:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly as px
-#import plotly.express as px
+import plotly.express as px
 import altair as alt
 from datetime import datetime, timedelta
 import numpy as np
